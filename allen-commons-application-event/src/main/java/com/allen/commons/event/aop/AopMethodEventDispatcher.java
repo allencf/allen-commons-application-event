71f47be357159439feb6fa6ceb0946f7075d2feb
chenfei1 <chenfei1@7F-Chenfei1.globale.com>
2016-12-02 16:39:16 +0800

    添加测试代码

--- allen-commons-application-event/src/main/java/com/allen/commons/event/aop/AopMethodEventDispatcher.java
+++ allen-commons-application-event/src/main/java/com/allen/commons/event/aop/AopMethodEventDispatcher.java
@@ -7,6 +7,9 @@ import org.aopalliance.intercept.MethodInvocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.allen.commons.event.annotation.ClassEvent;
+import com.allen.commons.event.annotation.MethodEvent;
+
 
 /**
  * 基于spring AOP的事件触发器
@@ -24,7 +27,17 @@ public class AopMethodEventDispatcher implements MethodInterceptor {
 	public Object invoke(MethodInvocation invocation) throws Throwable {
 		logger.info("execute AopMethodEventDispathcer invoke method -------------------------");
 		Method method = invocation.getMethod();
+		
+	    MethodEvent event = method.getAnnotation(MethodEvent.class);
+	    ClassEvent classEvent = method.getAnnotation(ClassEvent.class);
+	    if(event != null){
+	    	System.out.println("event is MethodEvent");
+	    }
+	    if(classEvent != null){
+	    	System.out.println("event is ClassEvent");
+	    }
 		System.out.println(method.getName());
+		System.out.println(event.toString());
 		
 		//执行被拦截的方法,切记,如果不返回次对象,则被拦截的方法不会被执行
 		Object object = invocation.proceed();
